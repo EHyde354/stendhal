dddcb568c451df87bafe33d490b031523f0b3948
EHyde354 <EBHyde354@outlook.com>
2025-03-28 14:43:42 +0000

    Update AGrandfathersWishTest.java

--- tests/games/stendhal/server/maps/quests/AGrandfathersWishTest.java
+++ tests/games/stendhal/server/maps/quests/AGrandfathersWishTest.java
@@ -10,7 +10,7 @@
  *                                                                         *
  ***************************************************************************/
 package games.stendhal.server.maps.quests;
-
+//weee
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
